de1cd2cf49fbd24d609230e99fc8798d55ca82a3
saisrivatsava <saisrivatsava.g@gmail.com>
2019-10-29 22:10:08 +0530

    improved

--- code_imageai.py
+++ code_imageai.py
@@ -2,34 +2,49 @@ from imageai.Detection import ObjectDetection
 from imageai.Prediction import ImagePrediction
 
 import os
+import argparse
+import sys
+
 import cv2
 
+# https://github.com/OlafenwaMoses/ImageAI/tree/master/imageai/Detection
 
 execution_path = os.getcwd()
-
-
 detector = ObjectDetection()
-# prediction = ImagePrediction()
-# prediction.setModelTypeAsResNet()
-# prediction.setModelPath(os.path.join(execution_path,"models/resnet50_weights_tf_dim_ordering_tf_kernels.h5"))
-# prediction.loadModel()
 
 
-detector.setModelTypeAsYOLOv3()
-detector.setModelPath(os.path.join(execution_path,"models/yolo.h5"))
-detector.loadModel()
+parser = argparse.ArgumentParser(description='Intruder Detection ...')
+parser.add_argument('--video', help='Video Name.')
+parser.add_argument('--model', help='Model Name.')
+args = vars(parser.parse_args())
 
 
-cap = cv2.VideoCapture(os.path.join(execution_path, "data/theft1.mp4"))
+if args["model"] == "yolo":
+    print("Using YOLO3 for Detection")
+    detector.setModelTypeAsYOLOv3()
+    detector.setModelPath(os.path.join(execution_path,"models/yolo.h5"))
+elif args["model"] == "yolo-tiny":
+    print("Using YOLO-TINY for Detection")
+    detector.setModelTypeAsTinyYOLOv3()
+    detector.setModelPath(os.path.join(execution_path,"models/yolo-tiny.h5"))
+elif args["model"] == "ratina":
+    print("Using RatinaNet for Detection")
+    detector.setModelTypeAsRetinaNet()
+    detector.setModelPath(os.path.join(execution_path,"models/resnet50_coco_best_v2.0.1.h5"))
+
+detector.loadModel(detection_speed="fast")
+
+
+cap = cv2.VideoCapture(os.path.join(execution_path, "data/{}.mp4".format(args["video"])))
 
 ret, frame1 = cap.read()
 ret, frame2 = cap.read()
 
-# frame1 = cv2.resize(frame1, (320, 320))
-# frame2 = cv2.resize(frame2, (320, 320))
+def drawPerson(frame, box):
+    # Draw a bounding box.
+    cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (255, 178, 50), 2)
+    return frame
 
-print(frame1.shape)
-print(frame2.shape)
 
 print("width : {}".format(cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
 print("height : {}".format(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
@@ -39,7 +54,7 @@ frame_num = 0
 while cap.isOpened():
     diff = cv2.absdiff(frame1, frame2)
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (15,15), 0)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
 
     _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
     dilated  = cv2.dilate(thresh, None, iterations=3)
@@ -56,13 +71,16 @@ while cap.isOpened():
         # predictions, probabilities = prediction.predictImage(frame_detected, result_count=10, input_type="array")
         custom_objects = detector.CustomObjects(person=True, motorcycle=True)
 
-        detections = detector.detectCustomObjectsFromImage(custom_objects=custom_objects, input_image=frame_detected,input_type="array",output_image_path=os.path.join(execution_path , "detected_img_{}.jpg".format(frame_num)),minimum_percentage_probability=60)
+        # detections = detector.detectCustomObjectsFromImage(custom_objects=custom_objects, input_image=frame_detected, input_type="array",output_image_path=os.path.join(execution_path , "detected/detected_img_{}.jpg".format(frame_num)),minimum_percentage_probability=60)
+
+        detected_image_array, detections = detector.detectCustomObjectsFromImage(output_type="array", custom_objects=custom_objects, input_image=frame_detected, input_type="array")#,minimum_percentage_probability=30)
+
 
         for eachObject in detections:
             print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
             print("--------------------------------")
-        #     if eachObject["name"] == "person" and eachObject["percentage_probability"] > 60: 
-        #         cv2.imshow("person", drawPreson(frame_detected, eachObject["box_points"]))
+            if eachObject["name"] == "person" and eachObject["percentage_probability"] > 30: 
+                cv2.imshow("person", drawPerson(detected_image_array, eachObject["box_points"]))
 
             
         frame_num= frame_num+1
@@ -94,10 +112,6 @@ cv2.destroyAllWindows()
 cap.release()
 
 
-def drawPreson(frame, box):
-    # Draw a bounding box.
-    cv.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (255, 178, 50), 2)
-    return frame
     
     # label = '%.2f' % conf
         
